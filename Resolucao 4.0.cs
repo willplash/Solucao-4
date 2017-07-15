03368dc34f09c5307dc05106033aa6de81dff414
willplash <willvwa@gmail.com>
2017-07-15 00:42:59 -0300

    Add files via upload

---
+++ Resolucao 4.0.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolucao_4._0
+{
+    class Program
+    {
+        public static double Average(int a, int b)
+        {
+            double aNovo, bNovo;
+            aNovo = System.Convert.ToDouble(a);
+            bNovo = System.Convert.ToDouble(b);
+            return (aNovo + bNovo) / 2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(2, 1));
+        }
+    }
+}
